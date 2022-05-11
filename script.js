b112864f4a3697d5eb94e0d5cb13059da0a816f7
Matthew <iam.matthewplus@gmail.com>
2022-05-10 22:20:53 -0300

    fixed border limits & updated system to automatically recognize image and frame size

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-  var         image = document.getElementsByClassName('my-img')[0],
-          imgCntnrs = document.getElementsByClassName('img-cntnr'),
-  dragImgMouseStart = {},
-           lastDiff = {x: 0, y: 0},
-         initialPos = image.getBoundingClientRect(),
-         currentPos = {x: -initialPos.width/2, y:0};
+const image = document.getElementsByClassName('my-img')[0],
+	imgContainer = document.getElementsByClassName('img-container')[0]
+
+let dragImgMouseStart = {},
+    lastDiff = {x: 0, y: 0},
+    initialPos = image.getBoundingClientRect(),
+    currentPos = {x: -initialPos.width/2, y:0};
+    imageProperties = {width: null, height: null},
+	containerProperties = {width: null, height: null}
+
+let containerStyle = getComputedStyle(imgContainer)
+
+image.onload = function() {
+    imageProperties.width = this.width
+	imageProperties.height = this.height
+	containerProperties.width = parseInt(containerStyle.width)
+	containerProperties.height = parseInt(containerStyle.height)
+
+	// Image and Container Properties
+    console.log(imageProperties)
+    console.log(containerProperties)
+}
 
   function mousedownDragImg(e) {
     e.preventDefault();
@@ -20,13 +36,43 @@
     e.preventDefault();
     lastDiff.x = e.clientX - dragImgMouseStart.x;
     lastDiff.y = e.clientY - dragImgMouseStart.y;
-    requestAnimationFrame(function(){
-                            image.style.transform = "translate(" + (currentPos.x + lastDiff.x) + "px," + (currentPos.y + lastDiff.y) + "px)";
-                          });
+    requestAnimationFrame(
+		function() {
+			const newPotions = { width: currentPos.x + lastDiff.x, height: currentPos.y + lastDiff.y }
+
+			// Left Border
+            if (newPotions.width > 1) {
+                newPotions.width = 0
+				currentPos.x = 0
+            } 
+
+			// Right Border
+            if (newPotions.width < ( containerProperties.width - imageProperties.width )) {
+				newPotions.width = containerProperties.width - imageProperties.width
+				currentPos.x = containerProperties.width - imageProperties.width
+            }
+
+			// Top Border
+			if (newPotions.height > 1) {
+				newPotions.height = 0
+				currentPos.y = 0
+			} 
+
+			// Bottom Border
+			if (newPotions.height < containerProperties.height - imageProperties.height) {
+				newPotions.height = containerProperties.height - imageProperties.height
+				currentPos.y = containerProperties.height - imageProperties.height
+			} 
+
+            image.style.transform = "translate(" + newPotions.width + "px," + newPotions.height + "px)";
+        }
+	);
   }
 
   function mouseupDragImg(e) {
     e.preventDefault();
+    // console.log(currentPos.x + lastDiff.x)
+   
     window.removeEventListener('mousemove', mousemoveDragImg);
     window.removeEventListener('mouseup', mouseupDragImg);
   }
